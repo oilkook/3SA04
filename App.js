65b835fcf04e87706905542b691e1b8c55b362ba
unknown <s6135512017@phuket.psu.ac.th>
2020-09-17 11:17:48 +0700

    3sa04-2

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Weather from './components/Weather'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
 
 export default function App() {
 
   return (
-    <View style={styles.container} >
-       <Weather zipCode="83120" /> 
-       <StatusBar style="auto" hidden={true}/>
+    <View style={styles.container}>
+      <Weather zipCode="90110" />
+      <StatusBar style="auto" />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    borderWidth: 5,
-    borderColor: 'red',    
-  }
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    justifyContent: 'center',
+  },
 })
--- components/Weather.js
+++ components/Weather.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import Forecast from './Forecast';
 
 export default function Weather(props) {
-
-  return (
-    <View  style={styles.container} >
-      <Text style={{ fontSize: 30 }}> Weather  {props.zipCode} </Text>
-      <View style={{width: 30, height: 30, backgroundColor: 'red'}}></View>     
-      <View style={{width: 30, height: 30, backgroundColor: 'blue'}}></View>     
-      <View style={{width: 30, height: 30, backgroundColor: 'green'}}></View>           
-    </View>
-  );
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    })
+    return (
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </ImageBackground>
+        </View>
+    );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-around',   
-    alignItems: 'center' ,
-    borderWidth: 5,
-    borderColor: 'red',    
-  }
-})
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+    
+});
